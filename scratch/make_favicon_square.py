b1e6e735e31c64801d2314b591231ae4ca9f5320
Aditya07024 <adityakumar07024@gmail.com>
2026-06-16 18:28:41 +0530

    resize favicon to standard 192x192 size

--- scratch/make_favicon_square.py
+++ scratch/make_favicon_square.py
@@ -13,5 +13,6 @@ y_offset = (max_dim - img.height) // 2
 square_img.paste(img, (x_offset, y_offset))
 
 # Save as favicon.png in frontend/public
-square_img.save('/Users/aditya/Downloads/MindGod/frontend/public/favicon.png')
-print(f"Successfully created square transparent favicon of size: {max_dim}x{max_dim}")
+resized_img = square_img.resize((192, 192), Image.Resampling.LANCZOS)
+resized_img.save('/Users/aditya/Downloads/MindGod/frontend/public/favicon.png')
+print(f"Successfully created square transparent favicon of size: 192x192")
